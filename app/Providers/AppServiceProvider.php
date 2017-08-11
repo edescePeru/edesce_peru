fbbdf6cce458bee9fb545e709118e6bf3430e202
arcadio1894 <joryes1894@gmail.com>
2017-08-11 17:20:40 -0500

    Https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -15,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (!App::environment('local')) {
+            URL::forceSchema('https');
+        }
     }
 
     /**
