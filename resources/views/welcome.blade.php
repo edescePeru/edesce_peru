18c030af3a2a108e5a3da4a566275fb286733150
arcadio1894 <joryes1894@gmail.com>
2018-03-22 19:02:19 -0500

    Fixing send email 20

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -943,7 +943,15 @@
                               <abbr title="Phone">P:</abbr> (044) 564358
                             </address>
 
-                            
+                            <div class="fb-page" data-href="https://www.facebook.com/EDESCE/?hc_ref=ARRJ6MglwibQcisL7VJwl2y1ymtXs64dtDuUN_Qyz1PLpZ5NWxhPeIlUGlVsbHocFwg&amp;fref=nf" data-tabs="messages" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/EDESCE/?hc_ref=ARRJ6MglwibQcisL7VJwl2y1ymtXs64dtDuUN_Qyz1PLpZ5NWxhPeIlUGlVsbHocFwg&amp;fref=nf" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/EDESCE/?hc_ref=ARRJ6MglwibQcisL7VJwl2y1ymtXs64dtDuUN_Qyz1PLpZ5NWxhPeIlUGlVsbHocFwg&amp;fref=nf">Edesce</a></blockquote></div>
+
+                            <script>(function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id)) return;
+                                    js = d.createElement(s); js.id = id;
+                                    js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.12';
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));</script>
                         </div>
                     </div>
                 </div>
